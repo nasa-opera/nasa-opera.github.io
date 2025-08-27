968ad181db135750a879f78a8fcac50a558cfcdd
Rishi Verma <riverma@users.noreply.github.com>
2025-08-27 10:28:28 -0700

    Update docusaurus.config.js
    Signed-off-by: Rishi Verma <riverma@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nasa-opera', // Update this to your GitHub org/user name.
-  projectName: 'website', // Update this to your repo name.
+  projectName: 'nasa-opera.github.io', // Update this to your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
